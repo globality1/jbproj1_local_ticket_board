8b069c574cbc48f9c290adf893e4bd3537419c90
globality1 <40605904+globality1@users.noreply.github.com>
2019-10-02 17:49:18 +0300

    deletion of kaki.js
    Uploaded by mistake to the project, irrelevant file

--- js/kaki.js
+++
@@ -1,4 +0,0 @@
-
-
-
-var eli = "<div class=" + merhaot + "col-sm noteDisplay" + marhaot + "id=" + merhaot + "noteNumber" + noteId + merhaor + "><div><b>" + noteContent + "</b><br/><br/><div id=" + merhaot + "noteContent" + merhaor + "><span></span></div></div><b>Target Date: </b>" + targetTime + "</br><b>Target Time: </b>" + targetTime + "</div>";
